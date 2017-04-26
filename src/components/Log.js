423497778afede0875f38bff65d07e0b441057f2
wnetking <wnetking>
2017-04-26 17:54:33 +0300

    add log component

---
+++ src/components/Log.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+// import ReactDOM from 'react-dom'
+
+export default class LogItem extends Component {
+  render() {
+    const {logContent} = this.props
+    return (
+      <p className='alert alert-alert'>
+        {logContent}
+      </p>
+    )
+  }
+}
--- src/components/Loger.js
+++ src/components/Loger.js
@@ -1,35 +1,41 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import LogItem from './Log'
 
 export default class Loger extends Component {
 
-    onSubmitForm() {
-        const { logers } = this.props
-        const newLogers = [].concat(logers, ReactDOM.findDOMNode(this.refs.newLog).value)
-        this.props.updateLogers(newLogers);
-        ReactDOM.findDOMNode(this.refs.newLog).value = ''
-    }
+  onSubmitForm() {
+    const {logers} = this.props
+    const newLogers = [].concat(ReactDOM.findDOMNode(this.refs.newLog).value, logers)
+    this.props.updateLogers(newLogers);
+    ReactDOM.findDOMNode(this.refs.newLog).value = ''
+  }
 
-    render() {
-        const { logers } = this.props
+  render() {
+    const {logers} = this.props
+    const logersItem = logers.map((item, index) => {
         return (
-            <div className='loger row'>
-                <div className='col-xs-5'>
-                    <div className='form-group'>
-                        <input type='email' ref='newLog' className='form-control form-control-lg' placeholder='Пил чай ...' />
-                        <small className='form-text text-muted'>Что делал сегодня?</small>
-                    </div>
-                    <button type='button' className='btn btn-primary' onClick={this.onSubmitForm.bind(this)}>Добавить</button>
-                </div>
-                <div className='col-xs-7'>
-                    <p className='alert alert-info'>
-                        {logers}
-                    </p>
-                </div>
-            </div>
+          <p><LogItem logContent={item} key={index}/></p>
         )
-    }
+      }
+    )
+    return (
+      <div className='loger row'>
+        <div className='col-xs-5'>
+          <div className='form-group'>
+            <input type='email' ref='newLog' className='form-control form-control-lg' placeholder='Пил чай ...'/>
+            <small className='form-text text-muted'>Что делал сегодня?</small>
+          </div>
+          <button type='button' className='btn btn-primary' onClick={this.onSubmitForm.bind(this)}>Добавить</button>
+        </div>
+        <div className='col-xs-7'>
+          <p className='alert alert-info'>
+            {logersItem}
+          </p>
+        </div>
+      </div>
+    )
+  }
 }
 
-Loger.propTypes = {
-}
+Loger.propTypes = {}
